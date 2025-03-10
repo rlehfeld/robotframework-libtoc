4c64509493b5c7bb944dff012b39cc81eaa0b79d
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2025-03-10 17:09:18 +0100

    add traces

--- robotframework_libtoc/libtoc.py
+++ robotframework_libtoc/libtoc.py
@@ -130,8 +130,11 @@ def add_files_from_folder(folder, base_dir_path, root=True):
         """
 
     content = [(i, os.path.abspath(os.path.join(folder, i))) for i in os.listdir(folder)]
+    print(f'{content=!r}', file=sys.__stderr__)
     dirs = [d for _, d in content if os.path.isdir(d)].sort()
+    print(f'{dirs=!r}', file=sys.__stderr__)
     files = [(i, f) for i, f in content if f.endswith('.html') and os.path.is_file(f, follow_symlinks=True)].sort()
+    print(f'{files=!r}', file=sys.__stderr__)
 
     for d in dirs:
         result_str += add_files_from_folder(
